76d5d9da381071c0a6dfc9d1badb9245d933f8eb
161184 <nasti.zaitseva@yandex.ru>
2019-03-21 12:18:58 +0300

    Zaitseva_PI3-1_Clicker

--- app/src/main/java/com/example/stargit2/MainActivity.java
+++ app/src/main/java/com/example/stargit2/MainActivity.java
@@ -2,17 +2,31 @@ package com.example.stargit2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
 
-    Button btn;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    TextView mainTextView;
+    Button btn1;
+    int count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn = findViewById(R.id.button);
+        btn1 = findViewById(R.id.btn);
+        btn1.setOnClickListener(this);
+
+        mainTextView = findViewById(R.id.main_textview);
     }
-}
+
+
+
+    @Override
+    public void onClick (View v){
+        mainTextView.setText(String.valueOf(++count));
+    }}
